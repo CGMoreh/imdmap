27ed02da75edc86ed9d43a5f9adda71a9078cec2
Chris Moreh <43904262+CGMoreh@users.noreply.github.com>
2023-09-13 01:08:18 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ if(Modernizr.webgl) {
 		  style: 'data/style.json', //stylesheet location
 			//style: 'https://s3-eu-west-1.amazonaws.com/tiles.os.uk/v2/styles/open-zoomstack-night/style.json',
 		  center: [-1.0807603987012158, 53.96546788682073], // starting position51.5074° N, 0.127850.910637,-1.27441
-		  zoom: 20, // starting zoom
+		  zoom: 17, // starting zoom
 		  minZoom:4,
 			maxZoom: 20, //
 		  attributionControl: false
